df504b2081166675589feecd67ec478bd1457d27
= <m.a.a.arango@hotmail.com>
2024-05-21 16:22:25 +0200

    categorizing Vlues

--- # Listas, Tuplas y Diccionarios.py
+++ # Listas, Tuplas y Diccionarios.py
@@ -10,3 +10,18 @@ print(mylistacompleta[2])
 
 mylistacompleta[2] = 'fresa'
 print('mylistacompleta')
+
+
+myTuplaFinal = ('manzana', 'banano', 'pera')
+print(myTuplaFinal)
+print(type(myTuplaFinal))
+print(myTuplaFinal[0])
+print(myTuplaFinal[1])
+print(myTuplaFinal[2])
+
+mydiccionario = {'aio' : 'banano', 'mdf' : 'manzana', 'los' : 'pera'}
+print(mydiccionario)
+print(type(mydiccionario))
+print(mydiccionario['aio'])
+print(mydiccionario['mdf'])
+print(mydiccionario['los'])
---
+++ #categorizing Vlues.py
@@ -0,0 +1,6 @@
+#categorizing Vlues
+
+mixvalores = [45, 258796, 25.874, True, 'texto, texto, texto', '45']
+
+for item in mixvalores:
+    print('{} es un tipo de dato {}'.format(item, type(item)))
