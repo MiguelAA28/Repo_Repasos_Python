6f6b5db390e1a69f89ea4fc1621f5ac34ecf912d
= <m.a.a.arango@hotmail.com>
2024-05-21 12:58:24 +0200

    Lista, Tuplas y Diccionarios

---
+++ # Listas, Tuplas y Diccionarios.py
@@ -0,0 +1,12 @@
+# Listas, Tuplas y Diccionarios
+
+mylistacompleta = ['manzana', 'banano', 'cereza']
+
+print(mylistacompleta)
+print(type(mylistacompleta))
+print(mylistacompleta[0])
+print(mylistacompleta[1])
+print(mylistacompleta[2])
+
+mylistacompleta[2] = 'fresa'
+print('mylistacompleta')
--- # String Data Type.py
+++ # String Data Type.py
@@ -2,3 +2,22 @@
 
 mystring = 'This is a string.'
 print(mystring)
+
+print(type(mystring))
+
+print(mystring + 'is of the data type' + str(type(mystring)))
+
+
+primerstring = 'water'
+segundostring = 'fall'
+tercerstring = primerstring + segundostring
+
+print(tercerstring)
+
+name = input ('Cual es su nombre?')
+print(name)
+
+color = input('cual es su color favorito?')
+animal = input('cual es su animal favorito')
+
+print('{}, es como un {} {}!'. format(name, color, animal))
