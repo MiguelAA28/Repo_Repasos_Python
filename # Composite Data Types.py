2228cc3015ac842f265f36f17e46ccb8470d87b7
= <m.a.a.arango@hotmail.com>
2024-05-22 11:05:13 +0200

    Composite Data Types

---
+++ # Composite Data Types.py
@@ -0,0 +1,8 @@
+# Composite Data Types
+
+MyVehicle = {'vin' : 'vacio', 'make' : 'vacio', 'model' : 'vacio', 'year' : '0', 'range' : ' 0 ', 'TopSpeed' : '0', 'ZeroSixty' : '0.0', 'mileage' : '0'}
+
+for key, value in MyVehicle.items():
+    print('{} : {}'.format(key, value))
+
+MyInventoryList = []
