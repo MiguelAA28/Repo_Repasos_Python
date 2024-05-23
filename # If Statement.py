eb0aed98871eaaabddc425b6e056f47b94dda176
= <m.a.a.arango@hotmail.com>
2024-05-23 11:04:12 +0200

    If Statement

---
+++ # If Statement.py
@@ -0,0 +1,11 @@
+# If Statement
+
+RespuestaUsuario = input('¿Necesitas Enviar Un Paquete? (ingrese yes, no o ninguno)')
+
+if RespuestaUsuario == 'yes':
+    print('Te podemos ayudar a cambiar este paquete!!!')
+elif RespuestaUsuario == 'no':
+    print('Haga una orden acontinuación para ser atendido')
+else:
+    print('Gracias por comunicarse, adios!!!')
+
